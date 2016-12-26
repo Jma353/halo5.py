e00d0e30af631ba50376003bff0d870e652b7fd4
Jma353 <jma353@cornell.edu>
2016-12-26 00:15:36 -0500

    All metadata requests

--- halo5/metadata.py
+++ halo5/metadata.py
@@ -11,3 +11,57 @@ class Metadata(Base):
 
   def commendations (self):
     return self.get (url = self.base_url + 'commendations')
+
+  def csr_designations (self):
+    return self.get (url = self.base_url + 'csr-designations')
+
+  def enemies (self):
+    return self.get (url = self.base_url + 'enemies')
+
+  def flexible_stats (self):
+    return self.get (url = self.base_url + 'flexible-stats')
+
+  def game_base_variants (self):
+    return self.get (url = self.base_url + 'game-base-variants')
+
+  def game_variants (self, id):
+    return self.get (url = self.base_url + 'game-variants/' + str(id))
+
+  def impulses (self):
+    return self.get (url = self.base_url + 'impulses')
+
+  def map_variants (self, id):
+    return self.get (url = self.base_url + 'map-variants/' + str(id))
+
+  def maps (self):
+    return self.get (url = self.base_url + 'maps')
+
+  def medals (self):
+    return self.get (url = self.base_url + 'medals')
+
+  def playlists (self):
+    return self.get (url = self.base_url + 'playlists')
+
+  def requisition_packs (self, id):
+    return self.get (url = self.base_url + 'requisition-packs/' + str(id))
+
+  def requisitions (self, id):
+    return self.get (url = self.base_url + 'requisitions/' + str(id))
+
+  def seasons (self):
+    return self.get (url = self.base_url + 'seasons')
+
+  def skulls (self):
+    return self.get (url = self.base_url + 'skulls')
+
+  def spartan_ranks (self):
+    return self.get (url = self.base_url + 'spartan-ranks')
+
+  def team_colors (self):
+    return self.get (url = self.base_url + 'team-colors')
+
+  def vehicles (self):
+    return self.get (url = self.base_url + 'vehicles')
+
+  def weapons (self):
+    return self.get (url = self.base_url + 'weapons')
