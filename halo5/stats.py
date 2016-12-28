ad48bbeca94b5dd7dc242868b502e98262b91378
Jma353 <jma353@cornell.edu>
2016-12-27 22:08:53 -0500

    Completion of Stats

--- halo5/stats.py
+++ halo5/stats.py
@@ -7,6 +7,18 @@ class Stats(Base):
   def __init__ (self, sub_key):
     super (Stats, self).__init__('stats/h5/', sub_key)
 
+  # Private
+  def _post_game (self, game_type, match_id):
+    full_url = self.base_url + game_type + '/matches/' + match_id
+    return self.get (url = full_url).json()
+
+  # Private
+  def _service_record (self, game_type, players = [], season_id = None):
+    full_url = self.base_url + 'servicerecords/' + game_type + '?'
+    params = { players : ','.join(players) }
+    if season_id is not None: params['seasonId'] = season_id
+    return self.get (params = params, url = full_url)
+
   def events_for_match (self, match_id):
     return self.get (
       url = self.base_url + 'matches/' + match_id + '/events').json()
@@ -14,5 +26,35 @@ class Stats(Base):
   def matches_for_player (self, player, modes = None, start = 0, count = 25):
     params = { 'start' : start, 'count' : count }
     if modes is not None: params['modes'] = modes
-    return self.get (
-      params = params, url = self.base_url + player + '/matches?').json()
+    full_url = self.base_url + player + '/matches?'
+    return self.get (params = params, url = full_url).json()
+
+  def player_leaderboard (self, title, season_id, playlist_id, count=200):
+    params = { 'count' : count }
+    full_url = ''.join ([self.base_url, 'player-leaderboards/csr/',
+      season_id, '/', playlist_id, '?'])
+    return self.get (params = params, url = full_url).json()
+
+  def post_game_arena (self, match_id):
+    return self._post_game('arena', match_id)
+
+  def post_game_campaign (self, match_id):
+    return self._post_game('campaign', match_id)
+
+  def post_game_custom (self, match_id):
+    return self._post_game ('custom', match_id)
+
+  def post_game_warzone (self, match_id):
+    return self._post_game('warzone', match_id)
+
+  def service_record_arena (self, players = [], season_id = None):
+    return self._service_record('arena', players, season_id)
+
+  def service_record_campaign (self, players = []):
+    return self._service_record('campaign', players)
+
+  def service_record_custom (self, players = []):
+    return self._service_record('custom', players)
+
+  def service_record_warzone (self, players = []):
+    return self._service_record('warzone', players)
